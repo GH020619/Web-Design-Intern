694a090516714ab91939f2b45ac0a37df93a00b5
GH020619 <sandy960424@gmail.com>
2024-01-29 16:40:34 +0800

    從FTP下載後push到github

---
+++ first_job/drpro/js/main.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+    $("#owlCarousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            }
+        }
+    });
+    $('.vote').click(function () {
+        $(this).addClass('voted');
+    })
+})
